94a1139983c890354eae86ba5fe83f6aea38fe57
sigilai <briansigilai@gmail.com>
2019-09-16 14:24:44 +0300

    changer currency to shillings

--- app/Product.php
+++ app/Product.php
@@ -12,7 +12,7 @@ class Product extends Model
     ];
 
     public function getPriceAttribute($value){
-        $newForm = '$'.$value;
+        $newForm = 'KSH'.$value;
         return $newForm;
     }
 
