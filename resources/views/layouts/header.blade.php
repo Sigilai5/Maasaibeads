d5e47427324a04430c9222923004123754f66f02
sigilai <briansigilai@gmail.com>
2019-10-10 15:00:54 +0300

    Change navbar to Maasaibeads

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>{{$title}}</title>
+    <title>Maasai Beads</title>
     <link rel="shortcut icon" href="images/home/logo.jpg" />
     <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
