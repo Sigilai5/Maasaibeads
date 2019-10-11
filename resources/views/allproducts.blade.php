a85ed86671e3c873cbeef499869e8cf211d80149
sigilai <briansigilai@gmail.com>
2019-10-11 15:33:41 +0300

    added categories

--- resources/views/allproducts.blade.php
+++ resources/views/allproducts.blade.php
@@ -130,7 +130,7 @@
             <div class="row">
                 <div class="col-sm-3">
                     <div class="left-sidebar">
-                        <h2>Category</h2>
+{{--                        <h2>Gender</h2>--}}
                         <div class="panel-group category-products" id="accordian"><!--category-productsr-->
                             <div class="panel panel-default">
                                 <div class="panel-heading">
@@ -142,19 +142,24 @@
                                     <h4 class="panel-title"><a href="{{route('womenProducts')}}">Women</a></h4>
                                 </div>
                             </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title"><a href="">Children</a></h4>
+                                </div>
+                            </div>
                         </div><!--/category-products-->
 
                         <div class="brands_products"><!--brands_products-->
-                            <h2>Brands</h2>
+                            <h2>Categories</h2>
                             <div class="brands-name">
                                 <ul class="nav nav-pills nav-stacked">
-                                    <li><a href="#"> <span class="pull-right">(50)</span>Acne</a></li>
-                                    <li><a href="#"> <span class="pull-right">(56)</span>Grüne Erde</a></li>
-                                    <li><a href="#"> <span class="pull-right">(27)</span>Albiro</a></li>
-                                    <li><a href="#"> <span class="pull-right">(32)</span>Ronhill</a></li>
-                                    <li><a href="#"> <span class="pull-right">(5)</span>Oddmolly</a></li>
-                                    <li><a href="#"> <span class="pull-right">(9)</span>Boudestijn</a></li>
-                                    <li><a href="#"> <span class="pull-right">(4)</span>Rösch creative culture</a></li>
+                                    <li><a href="#"> <span class="pull-right"></span>Perfumes</a></li>
+                                    <li><a href="#"> <span class="pull-right"></span>Jewellery</a></li>
+                                    <li><a href="#"> <span class="pull-right"></span>Beauty</a></li>
+                                    <li><a href="#"> <span class="pull-right"></span>Fashion</a></li>
+{{--                                    <li><a href="#"> <span class="pull-right">(5)</span>Oddmolly</a></li>--}}
+{{--                                    <li><a href="#"> <span class="pull-right">(9)</span>Boudestijn</a></li>--}}
+{{--                                    <li><a href="#"> <span class="pull-right">(4)</span>Rösch creative culture</a></li>--}}
                                 </ul>
                             </div>
                         </div><!--/brands_products-->
